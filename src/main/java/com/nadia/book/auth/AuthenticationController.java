e6482fda81779576ac8bf605dc2cce5744c3f7b7
Nadia Jazi <jazinadia7@gmail.com>
2024-04-14 02:55:10 +0200

    implementing the email service

--- src/main/java/com/nadia/book/auth/AuthenticationController.java
+++ src/main/java/com/nadia/book/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.nadia.book.auth;
 
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,7 +22,7 @@ public class AuthenticationController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<?> register(
             @RequestBody @Valid RegistrationRequest request
-    ){
+    ) throws MessagingException {
 
         service.register(request);
         return ResponseEntity.accepted().build();
