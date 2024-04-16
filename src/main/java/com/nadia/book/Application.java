acbded5320b279aa248aed378a346aca66506db5
Nadia Jazi <jazinadia7@gmail.com>
2024-04-16 02:36:13 +0200

    testing the registration

--- src/main/java/com/nadia/book/Application.java
+++ src/main/java/com/nadia/book/Application.java
@@ -1,8 +1,12 @@
 package com.nadia.book;
 
+import com.nadia.book.role.Role;
+import com.nadia.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -19,4 +23,14 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
 }
